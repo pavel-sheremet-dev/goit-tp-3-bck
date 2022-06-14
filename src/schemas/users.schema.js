c93e3427b9e23ad48a6a5ecca880365f9d1b68e8
Pavel Sheremet <pashqa.sheremet@gmail.com>
2022-06-14 20:01:56 +0300

    add pattern

--- src/schemas/users.schema.js
+++ src/schemas/users.schema.js
@@ -3,21 +3,15 @@ const joi = require('joi');
 const signup = joi.object({
   name: joi
     .string()
-    .pattern(/^[A-Za-zА-Яа-яґҐЁёІіЇїЄє'’ʼ\s-]{3,30}$/)
+    .pattern(/^[A-Za-zА-Яа-яґҐЁёІіЇїЄє'’ʼ\\s-]{5,30}$/)
     .required(),
   email: joi.string().email().required(),
-  password: joi
-    .string()
-    .pattern(/^[0-9a-zA-Z_\s'’ʼ-]{8,20}$/)
-    .required(),
+  password: joi.string().required(),
 });
 
 const signing = joi.object({
   email: joi.string().email().required(),
-  password: joi
-    .string()
-    .pattern(/^[0-9a-zA-Z_\s'’ʼ-]{8,20}$/)
-    .required(),
+  password: joi.string().required(),
 });
 
 const verify = joi.object({
